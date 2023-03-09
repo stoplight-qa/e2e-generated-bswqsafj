a1247f985c8fc4fb0616c5422741dc4d7b16b591
qa-bievu-1-ypis@example.com <qa-bievu-1-ypis@example.com>
2023-03-09 18:38:47 +0000

    First commit of promoted project

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://integration.stoplight-dev.com/api/v1/projects/cHJqOjEzNDg4Mw/spectral.js?branch_slug=main&token=a6360764-d7f1-4b70-af81-7cb5023746cf";
+export default { extends: ruleset };
